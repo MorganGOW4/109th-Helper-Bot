cf4af4fcd69619a8a26d38bf25dc8fbd538b19a0
MorganGOW4 <morgangow4@gmail.com>
2026-08-27 21:27:28 +0100

    Created /log command to log events, removed usless test commands.

--- bot.py
+++ bot.py
@@ -6,19 +6,29 @@ from dotenv import load_dotenv
 # Load secret token from your hidden .env file
 load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+ 
+class MyBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True # Required to read messages
+        super().__init__(command_prefix="!", intents=intents)
 
-# Set up bot intents (permissions)
-intents = discord.Intents.default()
-intents.message_content = True  # Required to read messages
+    # The setup hook is called before the bot connects to Discord
+    async def setup_hook(self):
+        # Loop through all files in the cogs directory
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py"):
+                # Load the cog using dot notation (e.g., cogs.general)
+                await self.load_extension(f"cogs.{filename[:-3]}")
+                print(f"Loaded extension: {filename[:-3]}")
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = MyBot()
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}!")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
+    guild_object = discord.Object(id=1542137386375319642)
+    bot.tree.copy_global_to(guild=guild_object)
+    await bot.tree.sync(guild=guild_object)
 
 bot.run(TOKEN)
